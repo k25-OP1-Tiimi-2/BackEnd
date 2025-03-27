9cbc6e6acd2bdf62d44736288d0de72414a5054c
ElliPiilonen <ellipiilonen@gmail.com>
2025-03-27 16:03:45 +0200

    lisätty manufacturer luokka

---
+++ tiimityo/src/main/java/com/backend/projekti/tiimityo/domain/Manufacturer.java
@@ -0,0 +1,53 @@
+package com.backend.projekti.tiimityo.domain;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+@Entity
+public class Manufacturer {
+    @Id
+    @GeneratedValue(strategy = GeneratedValue.AUTO)
+    private Long id;
+    private String name;
+
+    @JsonIgnore
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
+    private List<Product> products;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+    @Override
+    public String toString() {
+        return "Manufacturer [name=" + name + ", products=" + products + "]";
+    }
+
+}
---
+++ tiimityo/src/main/java/com/backend/projekti/tiimityo/domain/ManufacturerRepository.java
@@ -0,0 +1,7 @@
+package com.backend.projekti.tiimityo.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ManufacturerRepository extends CrudRepository<Manufacturer, Long> {
+
+}
