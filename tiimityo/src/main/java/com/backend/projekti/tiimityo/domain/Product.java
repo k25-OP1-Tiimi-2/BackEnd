3e2e1d88be9a102a2bc371bb632f3efc03342200
Joona Koskinen <joonakoskinen03@gmail.com>
2025-03-27 16:15:41 +0200

    product tiedot

--- tiimityo/src/main/java/com/backend/projekti/tiimityo/domain/Product.java
+++ tiimityo/src/main/java/com/backend/projekti/tiimityo/domain/Product.java
@@ -2,6 +2,8 @@ package com.backend.projekti.tiimityo.domain;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class Product {
@@ -12,52 +14,117 @@ public class Product {
     private String title;
     private double price;
     private String isbn;
+    private String type;
+    private String color;
+    private String size;
+
+    @ManyToOne
+    @JoinColumn(name = "manuid")
+    private Manufacturer manufacturer;
+
 
     public Product () {}
 
-    public Product(Long id, String title, double price, String isbn) {
+
+    public Product(Long id, String title, double price, String isbn, String type, String color, String size,
+            Manufacturer manufacturer) {
         this.id = id;
         this.title = title;
         this.price = price;
         this.isbn = isbn;
+        this.type = type;
+        this.color = color;
+        this.size = size;
+        this.manufacturer = manufacturer;
     }
+
 
     public Long getId() {
         return id;
     }
 
+
     public void setId(Long id) {
         this.id = id;
     }
 
-    public String gettitle() {
+
+    public String getTitle() {
         return title;
     }
 
-    public void settitle(String title) {
+
+    public void setTitle(String title) {
         this.title = title;
     }
 
-    public double getprice() {
+
+    public double getPrice() {
         return price;
     }
 
-    public void setprice(double price) {
+
+    public void setPrice(double price) {
         this.price = price;
     }
+
 
     public String getIsbn() {
         return isbn;
     }
 
+
     public void setIsbn(String isbn) {
         this.isbn = isbn;
     }
 
-    @Override
-    public String toString() {
-        return "Product [id=" + id + ", title=" + title + ", price=" + price + ", isbn=" + isbn + "]";
+
+    public String getType() {
+        return type;
     }
 
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+
+    public String getColor() {
+        return color;
+    }
+
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+
+    public String getSize() {
+        return size;
+    }
+
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+
+    public Manufacturer getManufacturer() {
+        return manufacturer;
+    }
+
+
+    public void setManufacturer(Manufacturer manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Product [id=" + id + ", title=" + title + ", price=" + price + ", isbn=" + isbn + ", type=" + type
+                + ", color=" + color + ", size=" + size + ", manufacturer=" + manufacturer + "]";
+    }
+
+ 
     
 }
