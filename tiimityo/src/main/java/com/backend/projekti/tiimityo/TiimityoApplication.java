02b6befe85d32a5fefd8e5124c5a5dfcbb4b27ac
RekoLep <reko.leppanen@myy.haaga-helia.fi>
2025-03-27 14:37:38 +0200

    testi

---
+++ tiimityo/src/main/java/com/backend/projekti/tiimityo/TiimityoApplication.java
@@ -0,0 +1,13 @@
+package com.backend.projekti.tiimityo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiimityoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiimityoApplication.class, args);
+	}
+
+}
---
+++ tiimityo/src/test/java/com/backend/projekti/tiimityo/TiimityoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.projekti.tiimityo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiimityoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
