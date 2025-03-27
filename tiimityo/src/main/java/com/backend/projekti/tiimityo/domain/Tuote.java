25806889810b6d7edfcbea4dc07001b41e5ec3ad
Prshkv <niko.prshkv@gmail.com>
2025-03-27 15:45:43 +0200

    small fixes

--- tiimityo/src/main/java/com/backend/projekti/tiimityo/domain/Tuote.java
+++ tiimityo/src/main/java/com/backend/projekti/tiimityo/domain/Tuote.java
@@ -1,18 +1,24 @@
 package com.backend.projekti.tiimityo.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Tuote {
 
+    @Id
     private Long id;
-    private String nimi;
-    private double hinta;
+
+    private String title;
+    private double price;
     private String isbn;
 
     public Tuote () {}
 
-    public Tuote(Long id, String nimi, double hinta, String isbn) {
+    public Tuote(Long id, String title, double price, String isbn) {
         this.id = id;
-        this.nimi = nimi;
-        this.hinta = hinta;
+        this.title = title;
+        this.price = price;
         this.isbn = isbn;
     }
 
@@ -24,20 +30,20 @@ public class Tuote {
         this.id = id;
     }
 
-    public String getNimi() {
-        return nimi;
+    public String gettitle() {
+        return title;
     }
 
-    public void setNimi(String nimi) {
-        this.nimi = nimi;
+    public void settitle(String title) {
+        this.title = title;
     }
 
-    public double getHinta() {
-        return hinta;
+    public double getprice() {
+        return price;
     }
 
-    public void setHinta(double hinta) {
-        this.hinta = hinta;
+    public void setprice(double price) {
+        this.price = price;
     }
 
     public String getIsbn() {
@@ -50,7 +56,7 @@ public class Tuote {
 
     @Override
     public String toString() {
-        return "Tuote [id=" + id + ", nimi=" + nimi + ", hinta=" + hinta + ", isbn=" + isbn + "]";
+        return "Tuote [id=" + id + ", title=" + title + ", price=" + price + ", isbn=" + isbn + "]";
     }
 
     
