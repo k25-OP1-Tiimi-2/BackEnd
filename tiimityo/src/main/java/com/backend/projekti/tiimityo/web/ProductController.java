a1107662ea7f6d4868957a0a5bef254fb1789b99
niklasovaska <niklas.ovaska@gmail.com>
2025-03-28 12:00:45 +0200

    getmapping kaikille tuotteille

--- tiimityo/src/main/java/com/backend/projekti/tiimityo/web/ProductController.java
+++ tiimityo/src/main/java/com/backend/projekti/tiimityo/web/ProductController.java
@@ -3,6 +3,7 @@ package com.backend.projekti.tiimityo.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -17,6 +18,15 @@ public class ProductController {
     private ProductRepository prepository;
     @Autowired
     private ManufacturerRepository mrepository;
+
+    //Get all products:
+    @GetMapping("/productlist")
+    public String getProductList(Model model) {
+        model.addAttribute("products", prepository.findAll());
+        model.addAttribute("manufacturers", mrepository.findAll());
+        return "productlist";
+    }
+    
 
     // Adding manufacturer:
     @RequestMapping("/addmanufacturer")
