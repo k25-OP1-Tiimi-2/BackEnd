56aee48a4d266640de261bc642ee8dec28654cbd
ElliPiilonen <ellipiilonen@gmail.com>
2025-03-29 17:03:23 +0200

    lisätty edit ja save product

--- tiimityo/src/main/java/com/backend/projekti/tiimityo/web/ProductController.java
+++ tiimityo/src/main/java/com/backend/projekti/tiimityo/web/ProductController.java
@@ -4,11 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.backend.projekti.tiimityo.domain.Manufacturer;
 import com.backend.projekti.tiimityo.domain.ManufacturerRepository;
+import com.backend.projekti.tiimityo.domain.Product;
 import com.backend.projekti.tiimityo.domain.ProductRepository;
 
 @Controller
@@ -19,16 +21,30 @@ public class ProductController {
     @Autowired
     private ManufacturerRepository mrepository;
 
-    //Get all products:
+    // Get all products:
     @GetMapping("/productlist")
     public String getProductList(Model model) {
         model.addAttribute("products", prepository.findAll());
         model.addAttribute("manufacturers", mrepository.findAll());
         return "productlist";
     }
-    
 
-    // Adding manufacturer:
+    // Edit product:
+    @RequestMapping("edit/{id}")
+    public String editProduct(@PathVariable("id") Long productId, Model model) {
+        model.addAttribute("product", prepository.findById(productId));
+        model.addAttribute("manufacturers", mrepository.findAll());
+        return "editproduct";
+    }
+
+    // Save product:
+    @PostMapping("/saveproduct")
+    public String save(Product product) {
+        prepository.save(product);
+        return "redirect:/productlist";
+    }
+
+    // Add manufacturer:
     @RequestMapping("/addmanufacturer")
     public String addManufacturer(Model manu) {
         manu.addAttribute("manufacturer", new Manufacturer());
@@ -36,7 +52,7 @@ public class ProductController {
         return "addmanufacturer";
     }
 
-    // Saving manufacturer:
+    // Save manufacturer:
     @PostMapping("/savemanufacturer")
     public String saveManufacturer(Manufacturer manufacturer) {
         mrepository.save(manufacturer);
