fd53bf0dfd12c482213d3680620f2489805bf07f
ElliPiilonen <ellipiilonen@gmail.com>
2025-04-05 01:49:38 +0300

    added findbymanufacturer

--- tiimityo/src/main/java/com/backend/projekti/tiimityo/domain/ManufacturerRepository.java
+++ tiimityo/src/main/java/com/backend/projekti/tiimityo/domain/ManufacturerRepository.java
@@ -1,7 +1,9 @@
 package com.backend.projekti.tiimityo.domain;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface ManufacturerRepository extends CrudRepository<Manufacturer, Long> {
-
+    List<Manufacturer> findByName(String name);
 }
--- tiimityo/src/main/java/com/backend/projekti/tiimityo/domain/ProductRepository.java
+++ tiimityo/src/main/java/com/backend/projekti/tiimityo/domain/ProductRepository.java
@@ -1,12 +1,12 @@
 package com.backend.projekti.tiimityo.domain;
 
-
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
 
-
-
 public interface ProductRepository extends CrudRepository<Product, Long> {
     List<Product> findByTitle(String title);
+
+    List<Product> findByManufacturer(Manufacturer manufacturer);
+
 }
