2138d79f2ad90503608e96146779a4a0478f4f12
niklasovaska <niklas.ovaska@gmail.com>
2025-04-07 11:57:15 +0300

    data rest dependecy -  products, manufacturers, findByType endpoints

--- tiimityo/src/main/java/com/backend/projekti/tiimityo/domain/ProductRepository.java
+++ tiimityo/src/main/java/com/backend/projekti/tiimityo/domain/ProductRepository.java
@@ -3,10 +3,13 @@ package com.backend.projekti.tiimityo.domain;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface ProductRepository extends CrudRepository<Product, Long> {
-    List<Product> findByTitle(String title);
+
+    List<Product> findByTitle(@Param("title") String title);
 
     List<Product> findByManufacturer(Manufacturer manufacturer);
 
+    List<Product> findByType(@Param("type") String type);
 }
