17f0b1bbe6ee296852de32c45bde9784265ec699
niklasovaska <niklas.ovaska@gmail.com>
2025-05-08 09:38:59 +0300

    productcontroller smoke test

--- tiimityo/src/test/java/com/backend/projekti/tiimityo/TiimityoApplicationTests.java
+++ tiimityo/src/test/java/com/backend/projekti/tiimityo/TiimityoApplicationTests.java
@@ -1,13 +1,22 @@
 package com.backend.projekti.tiimityo;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.backend.projekti.tiimityo.web.ProductController;
 
 @SpringBootTest
 class TiimityoApplicationTests {
 
+	@Autowired
+	private ProductController pcontroller;
+
 	@Test
-	void contextLoads() {
+	public void contextLoads() throws Exception {
+		assertThat(pcontroller).isNotNull();
 	}
 
 }
